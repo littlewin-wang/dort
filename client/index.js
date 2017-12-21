d34983575178c725d176a4bf91c2022750cca9fd
littlewin <littlewin.wang@gmail.com>
2017-12-21 23:38:08 +0800

    update file watch

---
+++ client/index.js
@@ -0,0 +1,105 @@
+/**
+ * 用户端入口文件
+ */
+
+const colors = require('colors')
+const ip = require('ip')
+
+const Watch = require('./watch')
+
+const packageConfig = require('../package.json')
+
+class App {
+  constructor() {
+    console.log('---'.rainbow + ' Dort '.bold + packageConfig.version.bold + ' ----------------------'.rainbow)
+
+    // 导入配置参数
+    this.setConfig()
+
+    // 开启监控模块
+    this.setWatch()
+  }
+
+  // 解析命令行参数
+  setConfig () {
+    const config = {}
+
+    const args = require('yargs')
+      .option('debug', {
+        alias: 'd',
+        describe: '开发调试等级，用来规范打印信息',
+        default: 1,
+        type: 'number'
+      })
+      .option('name', {
+        alias: 'n',
+        describe: '工程名称 ',
+        default: '',
+        type: 'string'
+      })
+      .option('port', {
+        alias: 'p',
+        describe: '服务端口',
+        default: 4574,
+        type: 'number'
+      })
+      .option('exclude', {
+        alias: 'e',
+        describe: '需要排除掉的文件',
+        default:
+          [
+            '**/.DS_Store',
+            'node_modules/**',
+            'vendor/**',
+            '.git'
+          ],
+        type: 'array'
+      })
+      .option('test', {
+        alias: 't',
+        describe: '开启测试工程',
+        default: false,
+        type: 'boolean'
+      })
+      .option('max-file-size', {
+        alias: 'm',
+        describe: '单文件大小限制',
+        default: 99999,
+        type: 'number'
+      })
+      .argv
+
+    config.debug = args.debug
+    config.name = args.name
+    config.port = args.port
+    config.exclude = args.exclude
+    config.test = args.test
+
+    // 工程名单独处理，可以是第一个arguments参数
+    if (config.name === '') {
+      if (args._.length > 0 && typeof args._[0] === 'string' && args._[0] !== '') {
+        config.name = args._[0]
+      } else {
+        config.name = 'DEFAULT'
+      }
+    }
+
+    // 定义domain
+    config.domain = `http://${ip.address()}${config.port !== 80 ? ':' + config.port : ''}`
+
+    this.config = config
+
+    if (this.config.debug >= 1) {
+      for (const _configKey in this.config) {
+        const _configValue = '' + this.config[_configKey]
+        console.log('[config]'.yellow.bold + ' - ' + '<' + _configKey.green + '>' + ': ' + _configValue.cyan)
+      }
+    }
+  }
+
+  setWatch () {
+    this.watch = new Watch(this.config)
+  }
+}
+
+const app = new App()
---
+++ client/watch.js
@@ -0,0 +1,173 @@
+/**
+ * 文件监控模块
+ */
+
+const chokidar = require('chokidar')
+const fs = require('fs')
+const mime = require('mime')
+const socketIoClient = require('socket.io-client')
+
+class Watch {
+  constructor(_config) {
+    this.config = _config
+    this.path = process.cwd()
+
+    this.setExcludeRegex()
+    this.setSocket()
+    this.watch()
+  }
+
+  // 初始化socket
+  setSocket () {
+    this.socket = socketIoClient(`${this.config.domain}/watch`)
+
+    this.socket.on('connect', () => {
+      this.socket.emit('start_project', { name: this.config.name, path: this.path, excludeRegex: '' + this.excludeRegex })
+
+      if (this.config.debug) {
+        console.log('[watch] > socket'.green.bold + ' - ' + 'connect'.cyan)
+      }
+    })
+
+    this.socket.on('disconnect', () => {
+      if (this.config.debug) {
+        console.log('[watch] > socket'.green.bold + ' - ' + 'disconnect'.cyan)
+      }
+    })
+  }
+
+  setExcludeRegex () {
+    const regexs = []
+    const Minimatch = require('minimatch').Minimatch
+
+    Object.keys(this.config.exclude).forEach(key => {
+      const value = this.config.exclude[key]
+      const minimatch = new Minimatch(value, { dot: true })
+
+      let regex = '' + minimatch.makeRe()
+      regex = regex.replace('/^', '')
+      regex = regex.replace('$/', '')
+
+      regexs.push(regex)
+    })
+
+    this.excludeRegex = new RegExp(regexs.join('|'))
+  }
+
+  watch () {
+    this.watch = chokidar.watch(
+      this.path,
+      {
+        ignored: this.excludeRegex
+        // ignoreInitial: !this.config.initialSend
+      }
+    )
+
+    if (this.config.debug >= 1) {
+      console.log('[watch]'.green.bold + ' - ' + 'start watching'.cyan + ' - ' + this.path.cyan)
+    }
+
+    // 添加文件事件响应函数
+    this.watch.on('add', (_path) => {
+      const relativePath = _path.replace(this.path, '.')
+      const mimeType = mime.getType(relativePath)
+      const file = {}
+
+      file.path = relativePath
+      file.canRead = true
+
+      // 媒体文件不可读
+      if (mimeType.match(/^(audio)|(video)|(image)/)) {
+        file.canRead = false
+      }
+
+      fs.stat(_path, (error, stats) => {
+        // 判断文件大小
+        if (this.config.maxFileSize < stats.size) {
+          file.canRead = false
+        }
+
+        fs.readFile(_path, (error, data) => {
+          if (file.canRead) {
+            file.content = data.toString()
+          }
+
+          this.socket.emit('create_file', file)
+        })
+      })
+
+      if (this.config.debug >= 1) {
+        console.log('[watch]'.green.bold + ' - ' + 'add'.cyan + ' - ' + relativePath.cyan)
+      }
+    })
+
+    // 更新文件事件响应函数
+    this.watch.on('change', (_path) => {
+      const relativePath = _path.replace(this.path, '.')
+      const mimeType = mime.getType(relativePath)
+      const file = {}
+
+      file.path = relativePath
+      file.canRead = true
+
+      // 媒体文件不可读
+      if (mimeType.match(/^(audio)|(video)|(image)/)) {
+        file.canRead = false
+      }
+
+      fs.stat(_path, (error, stats) => {
+        // 判断文件大小
+        if (this.config.maxFileSize < stats.size) {
+          file.canRead = false
+        }
+
+        fs.readFile(_path, (error, data) => {
+          if (file.canRead) {
+            file.content = data.toString()
+          }
+
+          this.socket.emit('update_file', file)
+        })
+      })
+
+      if (this.config.debug >= 1) {
+        console.log('[watch]'.green.bold + ' - ' + 'change'.cyan + ' - ' + relativePath.cyan)
+      }
+    })
+
+    // 删除文件事件响应函数
+    this.watch.on('unlink', (_path) => {
+      const relativePath = _path.replace(this.path, '.')
+
+      this.socket.emit('delete_file', { path: relativePath })
+
+      if (this.config.debug >= 1) {
+        console.log('[watch]'.green.bold + ' - ' + 'unlink'.cyan + ' - ' + relativePath.cyan)
+      }
+    })
+
+    // 添加文件夹事件响应函数
+    this.watch.on('addDir', (_path) => {
+      const relativePath = _path.replace(this.path, '.')
+
+      this.socket.emit('create_folder', { path: relativePath })
+
+      if (this.config.debug >= 1) {
+        console.log('[watch]'.green.bold + ' - ' + 'addDir'.cyan + ' - ' + relativePath.cyan)
+      }
+    })
+
+    // 删除文件夹事件响应函数
+    this.watch.on('unlinkDir', (_path) => {
+      const relativePath = _path.replace(this.path, '.')
+
+      this.socket.emit('delete_folder', { path: relativePath })
+
+      if (this.config.debug >= 1) {
+        console.log('[watch]'.green.bold + ' - ' + 'unlinkDir'.cyan + ' - ' + relativePath.cyan)
+      }
+    })
+  }
+}
+
+module.exports = Watch
