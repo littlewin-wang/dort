66b4ab5b3f0d01abd220ac79765ee7e01057daef
littlewin <littlewin.wang@sky-cloud.net>
2018-05-04 15:21:59 +0800

    feature: add messages list

--- client/src/components/Chat/index.vue
+++ client/src/components/Chat/index.vue
@@ -7,7 +7,18 @@
       </h5>
     </div>
     <div class="box" v-if="open">
-      <div class="content"></div>
+      <div class="content">
+        <div class="item" v-for="msg in messages" :key="msg.id">
+          <div class="state" :style="{background: msg.user.color}"></div>
+          <div class="container">
+            <div class="param">
+              <span class="name" :style="{color: msg.user.color}">{{msg.user.name}}</span>
+              <span class="time">{{new Date(msg.time).toLocaleString()}}</span>
+            </div>
+            <span class="text">{{msg.text}}</span>
+          </div>
+        </div>
+      </div>
       <div class="input">
         <div class="name">
           <span>Nickname assign to you is </span>
@@ -33,7 +44,7 @@ export default {
     }
   },
   computed: {
-    ...mapGetters('chat', ['open', 'user']),
+    ...mapGetters('chat', ['open', 'user', 'messages']),
     user () {
       return this.$store.state.chat.user
     }
@@ -89,10 +100,44 @@ export default {
     }
   }
   .box {
+    width: 320px;
     .content {
       height: 300px;
+      margin-right: -15px;
+      padding: .5rem calc(0.5rem + 15px) .5rem .5rem;
       overflow-y: auto;
       background: #2c2a42;
+      .item {
+        display: flex;
+        justify-content: space-between;
+        .state {
+          flex: 0 0 3px;
+        }
+        .container {
+          margin-left: .5rem;
+          flex: 1;
+          overflow: hidden;
+          font-size: 14px;
+          font-weight: 300;
+          .param {
+            display: flex;
+            justify-content: space-between;
+            .time {
+              font-size: 12px;
+              opacity: .6
+            }
+          }
+          .text {
+            width: 100%;
+            margin-top: .5rem;
+            font-size: 13px;
+            white-space: pre-wrap;
+          }
+        }
+        &:not(:first-child) {
+          margin-top: 1rem;
+        }
+      }
     }
     .input {
       padding: .2rem .5rem;
